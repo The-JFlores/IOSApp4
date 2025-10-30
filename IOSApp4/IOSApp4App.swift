e622d43e0a4dabca540cb6b10487cf794e43124d
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2025-10-30 09:37:26 -0400

    Initial Commit

---
+++ IOSApp4/IOSApp4App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp4App.swift
+//  IOSApp4
+//
+//  Created by Jose Flores on 2025-10-30.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
